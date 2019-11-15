0395325ef126c2415141d1ab04c81b2aa6b4221c
AerialX <aerialx@users.noreply.github.com>
2019-11-14 16:39:03 -0800

    Only use write syscalls with logger

--- src/macros.rs
+++ src/macros.rs
@@ -1,8 +1,6 @@
+#[doc(hidden)]
 #[macro_export]
-macro_rules! print {
-    ($str:expr) => {
-        $crate::print_cstr($crate::_export::cstr!($str).into())
-    };
+macro_rules! print_fmt {
     ($($tt:tt)*) => {
         {
             use $crate::_export::core::fmt::Write;
@@ -12,15 +10,87 @@ macro_rules! print {
     };
 }
 
+#[doc(hidden)]
 #[macro_export]
-macro_rules! println {
-    ($str:expr) => {
-        $crate::print_cstr($crate::_export::cstr!(concat!($str, "\n")).into())
-    };
+macro_rules! println_fmt {
     ($($tt:tt)*) => {
         {
             use $crate::_export::core::fmt::Write;
+            // failures aren't interesting to us
             let _ = $crate::_export::core::writeln!(&$crate::LOGGER, $($tt)*);
         }
+    };
+}
+
+#[doc(hidden)]
+#[cfg(feature = "enable-logger")]
+#[macro_export]
+macro_rules! print_str {
+    ($str:expr) => {
+        {
+            let _ = $crate::_export::core::fmt::Write::write_str(&mut &$crate::LOGGER, $str);
+        }
+    };
+}
+
+#[doc(hidden)]
+#[cfg(feature = "enable-logger")]
+#[macro_export]
+macro_rules! println_str {
+    ($str:expr) => {
+        {
+            let _ = $crate::_export::core::fmt::Write::write_str(&mut &$crate::LOGGER, $str);
+            let _ = $crate::_export::core::fmt::Write::write_str(&mut &$crate::LOGGER, "\n");
+        }
+    };
+}
+
+#[doc(hidden)]
+#[cfg(not(feature = "enable-logger"))]
+#[macro_export]
+macro_rules! print_str {
+    ($str:literal) => {
+        $crate::print_cstr($crate::_export::cstr!($str).into())
+    };
+    ($str:expr) => {
+        $crate::print_fmt!("{}", $str)
+    };
+}
+
+#[doc(hidden)]
+#[cfg(not(feature = "enable-logger"))]
+#[macro_export]
+macro_rules! println_str {
+    ($str:literal) => {
+        $crate::print_cstr($crate::_export::cstr!(concat!($str, "\n")).into())
+    };
+    ($str:expr) => {
+        $crate::println_fmt!("{}", $str)
+    };
+}
+
+#[macro_export]
+macro_rules! print {
+    ($str:literal) => {
+        $crate::print_str!($str)
+    };
+    ($str:expr) => {
+        $crate::print_str!($str)
+    };
+    ($($tt:tt)*) => {
+        $crate::print_fmt!($($tt)*)
+    };
+}
+
+#[macro_export]
+macro_rules! println {
+    ($str:literal) => {
+        $crate::println_str!($str)
+    };
+    ($str:expr) => {
+        $crate::println_str!($str)
+    };
+    ($($tt:tt)*) => {
+        $crate::println_fmt!($($tt)*)
     };
 }
